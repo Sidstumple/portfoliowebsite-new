967bd7780cdb6cc3eed4910282609f2d42829e16
Sidstumple <cydstumpel@MacBook-Pro-van-Cyd.local>
2017-04-28 12:06:32 +0200

    update!

--- static/js/script.js
+++ static/js/script.js
@@ -2,9 +2,23 @@ var backgroundImage = document.getElementById('changing-background');
 var allImages       = document.querySelectorAll('.use');
 var lengthImages    = allImages.length;
 var randomNumber    = Math.floor(Math.random() * (lengthImages - 1));
-
-console.log(allImages);
+var workImages      = document.querySelectorAll('.werk-foto');
+var overlay         = document.getElementById('overlay');
+var overlayImage    = document.getElementById('overlay-image');
 
 for(var i = 0; i < allImages.length; i++) {
-  backgroundImage.style.backgroundImage = `url("${allImages[randomNumber].src}")`;
+  backgroundImage.style.background = `url("${allImages[randomNumber].src}")`;
 }
+
+// console.log(workImages);
+//
+// workImages.forEach(function(el) {
+//   console.log(el.id);
+//   document.getElementById(el.id).addEventListener('click', function(e) {
+//     var src = e.target.src;
+//     if(overlay.classList.contains('hide')) {
+//       overlay.classList.remove('hide');
+//       overlayImage.style.background = `url(${src})`;
+//     }
+//   })
+// });
